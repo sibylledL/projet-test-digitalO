7ceec750919267fc5c92554900f5ff69587a3cc8
sibylledL <s.delaurens@gmail.com>
2018-10-10 16:21:23 +0200

    change port

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true,}))
 
-const port = 4567;
+const port = 80;
 const dbUrl = "mongodb://localhost/imageTest_db";
 
 mongoose.connect(dbUrl)
